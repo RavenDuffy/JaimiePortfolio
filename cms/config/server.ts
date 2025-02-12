932a4374f7dc69a53befcdb1607377dd77bff7e4
Raven <raven@Ravens-MacBook-Pro.local>
2025-02-12 15:07:45 +0000

    ci: logging env to see

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,7 +1,11 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  console.log(env)
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+  }
+}
