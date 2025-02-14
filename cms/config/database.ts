30f095240d9faf3b5cefea6150af96f68647a802
Raven <raven@Ravens-MacBook-Pro.local>
2025-02-14 15:18:48 +0000

    fix: added public schema to db

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -61,17 +61,22 @@ export default ({ env }) => {
   const connection = {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
     useNullAsDefault: true,
   }
 
   return {
     connection: { ...connection },
+    defaultConnection: "default",
     acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    // ssl: {
+    //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+    // },
   }
 }
